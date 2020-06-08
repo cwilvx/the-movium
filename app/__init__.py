80930d6dd53c0f4cf205c32aaf6051cfdf35e9d3
geoffrey45 <geoffreymungai45@gmail.com>
2020-06-08 14:29:32 +0300

    deployment to heroku

--- app/__init__.py
+++ app/__init__.py
@@ -1,11 +1,19 @@
 from flask import Flask
-from .config import DevConfig
 from flask_bootstrap import Bootstrap
+from config import config_options
 
-app = Flask(__name__,instance_relative_config = True)
-app.config.from_object(DevConfig)
-app.config.from_pyfile('config.py')
-bootstrap = Bootstrap(app)
+bootstrap = Bootstrap()
 
-from app import views
-from app import error
+def create_app(config_name):
+	app = Flask(__name__)
+	app.config.from_object(config_options[config_name])
+	
+	bootstrap.init_app(app)
+
+	from .main import main as main_blueprint
+	app.register_blueprint(main_blueprint)
+
+	from .request import configure_request
+	configure_request(app)
+	
+	return app
--- app/error.py
+++
@@ -1,7 +0,0 @@
-from flask import render_template
-from app import app
-
-@app.errorhandler(404)
-def four_ow_four(error):
-    
-    return render_template('fourowfour.html')
