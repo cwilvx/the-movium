7aa4648a9408b1f8c082732a9baa209d2214dc96
geoffrey45 <geoffreymungai45@gmail.com>
2020-06-10 13:22:30 +0300

    add progresql database and create registration form

--- app/main/error.py
+++ app/main/error.py
@@ -1,8 +1,9 @@
 from flask import render_template
 from . import main
 
-
 @main.app_errorhandler(404)
 def four_ow_four(error):
-    
-    return render_template('fourowfour.html'),404
+    '''
+    Function to render the 404 error page
+    '''
+    return render_template('fourOwfour.html'),404
