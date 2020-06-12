8fb8f085b5714d2014d4c87368cb94704e10f6b6
geoffrey45 <geoffreymungai45@gmail.com>
2020-06-12 17:45:25 +0300

    add sending welcome email

--- app/main/views.py
+++ app/main/views.py
@@ -4,7 +4,7 @@ from ..request import get_movies,get_movie,search_movie
 from .forms import ReviewForm,UpdateProfile
 from ..models import Review, User
 from flask_login import login_required
-from .. import db
+from .. import db,photos
 
 @main.route('/user/<uname>',methods = ['GET','POST'])
 def profile(uname):
@@ -89,3 +89,13 @@ def update_profile(uname):
         
         return redirect(url_for('.profile',uname = user.username))
     return render_template('profile/update.html',form = form)
+
+@main.route('/user/<uname>/update/pic',methods = ['POST'])
+def update_pic(uname):
+    user = User.query.filter_by(username = uname).first()
+    if 'photo' in request.files:
+        filename = photos.save(request.files['photo'])
+        path = f'photos/{filename}'
+        user.profile_pic_path = path
+        db.session.commit()
+    return redirect(url_for('main.profile',uname = uname))
