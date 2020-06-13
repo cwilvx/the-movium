3cb072b8bd937057f4781b410d30bd12af929de0
geoffrey45 <geoffreymungai45@gmail.com>
2020-06-13 17:49:49 +0300

    Update mail credentials & 404 page

--- app/main/error.py
+++ app/main/error.py
@@ -3,7 +3,4 @@ from . import main
 
 @main.app_errorhandler(404)
 def four_ow_four(error):
-    '''
-    Function to render the 404 error page
-    '''
-    return render_template('fourOwfour.html'),404
+    return render_template('fourowfour.html'),404
